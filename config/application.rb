e7eb35a6a7ce1f4d54aa0e1546ae8fef469630a9
BillyMiles <williammiles201@gmail.com>
2022-12-08 15:49:15 -0500

    bundler reverted

--- config/application.rb
+++ config/application.rb
@@ -17,8 +17,7 @@ require "action_view/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-# Bundler.require(*Rails.groups)
-Bundler.require(:default, Rails.env)
+Bundler.require(*Rails.groups)
 
 module WeatherNewBackend
   class Application < Rails::Application
